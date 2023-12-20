35da42ec70eb9bd5634f672d219d0578a9a66e2c
sanyomor-01 <sanyomormichael@gmail.com>
2023-12-20 07:56:05 +0000

    Added a form

---
+++ myJs.js
@@ -0,0 +1,9 @@
+/*const inputField = document.querySelector('input');
+
+inputField.addEventListener('focus', () => {
+  inputField.style.borderColor = '#ff0000';
+});
+*/
+function handleChange (event) {
+  console.log(event.target)
+}
